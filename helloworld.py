76cfe003ca558ce0be40a76bd99337750ee47bed
Sean <officialseanstevens@gmail.com>
2018-10-05 12:04:04 -0500

    No update

--- helloworld.py
+++ helloworld.py
@@ -1,6 +1,15 @@
+<<<<<<< HEAD
 print ("Hello World!")
 
 for i in range(5):
     print (i)
 for i in range (1,6):
     print ("This is number " + str(i))
+=======
+print ("Hello World!")
+
+for i in range(5):
+    print (i)
+for i in range (1,6):
+    print ("This is number " + str(i))
+>>>>>>> 46c836ba28917ca6ef9e5e774ee3b17f7a193e2a
